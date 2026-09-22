0eb0f639dc96dc3641d6c41365bf6a34c5ee50f3
Danieli Fiel Reis <danielifiel99@gmail.com>
2026-09-22 10:42:10 -0300

    fix: ajusta pagina de mensagens

--- app/mensagens/page.tsx
+++ app/mensagens/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
